904e5c044896a325902ca6a86511ec351f3919e1
Anton Lorvi <lorvi.anton@gmail.com>
2019-04-18 22:55:00 +0200

    Firebase - registerRequest

---
+++ src/main/java/ch/fhnw/digibp/db/RequestDAO.java
@@ -0,0 +1,47 @@
+package ch.fhnw.digibp.db;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+
+public class RequestDAO extends FirestoreDBCon {
+    final private String COLLECTION = "requests";
+    private Firestore db = null;
+
+    public RequestDAO() {
+        this.db = super.getInstance();
+    }
+
+    public void registerRequest(String requestId,  Map<String, String> docData) throws InterruptedException, ExecutionException {
+        ApiFuture<WriteResult> future = db.collection(COLLECTION).document(requestId).set(docData);
+        System.out.println("Update time : " + future.get().getUpdateTime());
+    };
+
+    public void fulfillRequest() throws InterruptedException, ExecutionException {
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("status", "FULLFILLED");
+        docData.put("statusTimestamp", Timestamp.now().toString());
+        ApiFuture<WriteResult> future = db.collection(COLLECTION).document("requestId").update(docData);
+        System.out.println("Update time : " + future.get().getUpdateTime());
+    };
+
+    public void cancelRequest() throws InterruptedException, ExecutionException {
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("status", "CANCELLED");
+        docData.put("statusTimestamp", Timestamp.now().toString());
+        ApiFuture<WriteResult> future = db.collection(COLLECTION).document("requestId").update(docData);
+        System.out.println("Update time : " + future.get().getUpdateTime());
+    };
+
+    public boolean hasDublicate(String requestId) throws InterruptedException, ExecutionException {
+        ApiFuture<DocumentSnapshot> query = db.collection(COLLECTION).document(requestId).get();
+        return query.get().exists();
+    };
+
+}
