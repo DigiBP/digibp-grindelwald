16ee268a84a542814b778d6b440a5f88e4ef74e3
Anton Lorvi <lorvi.anton@gmail.com>
2019-04-19 00:44:02 +0200

    Request Management

--- src/main/java/ch/fhnw/digibp/db/FirestoreDBCon.java
+++ src/main/java/ch/fhnw/digibp/db/FirestoreDBCon.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
@@ -16,23 +15,24 @@ public abstract class FirestoreDBCon {
     private static final String SERVICE_ADDRESS = "https://grindelwald-638c8.firebaseio.com";
 
     public FirestoreDBCon() {
-        InputStream serviceAccount = this.getClass().getResourceAsStream(SA_FILE_PATH);
-        try {
-            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
-            //FirestoreOptions fsOptions = FirestoreOptions.newBuilder().setTimestampsInSnapshotsEnabled(true).build();
-            FirebaseOptions fbOptions = new FirebaseOptions.Builder()
-            .setCredentials(credentials)
-            .setDatabaseUrl(SERVICE_ADDRESS)
-            //.setFirestoreOptions(fsOptions)
-            .build();
-            FirebaseApp.initializeApp(fbOptions);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (FirebaseApp.getApps().isEmpty()) {
+            InputStream serviceAccount = this.getClass().getResourceAsStream(SA_FILE_PATH);
+            try {
+                GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+                //FirestoreOptions fsOptions = FirestoreOptions.newBuilder().setTimestampsInSnapshotsEnabled(true).build();
+                FirebaseOptions fbOptions = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .setDatabaseUrl(SERVICE_ADDRESS)
+                //.setFirestoreOptions(fsOptions)
+                .build();
+                FirebaseApp.initializeApp(fbOptions);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public Firestore getInstance(){
         return FirestoreClient.getFirestore();
     }
-
 }
--- src/main/java/ch/fhnw/digibp/db/RequestDAO.java
+++ src/main/java/ch/fhnw/digibp/db/RequestDAO.java
@@ -15,9 +15,7 @@ public class RequestDAO extends FirestoreDBCon {
     private Firestore db = null;
 
     public RequestDAO() {
-        if (this.db != null){
-            this.db = super.getInstance();
-        }
+        this.db = super.getInstance();
     }
 
     public void registerRequest(String requestId,  Map<String, String> docData) throws InterruptedException, ExecutionException {
