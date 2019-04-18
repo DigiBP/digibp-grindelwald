c9918dd1362d4619aa36fa52c558941c14157f0f
Anton Lorvi <lorvi.anton@gmail.com>
2019-04-19 00:53:30 +0200

    Request Management

--- src/main/java/ch/fhnw/digibp/rm/CancelRequestDelegate.java
+++ src/main/java/ch/fhnw/digibp/rm/CancelRequestDelegate.java
@@ -1,12 +1,5 @@
 package ch.fhnw.digibp.rm;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import com.google.cloud.Timestamp;
-
-import org.apache.commons.lang.StringUtils;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
@@ -18,18 +11,7 @@ public class CancelRequestDelegate implements JavaDelegate {
 
     public void execute(DelegateExecution execution) throws Exception {
         String businessKey = execution.getBusinessKey();
-        if(StringUtils.isEmpty(businessKey)){
-            businessKey = "R" + UUID.randomUUID().toString();
-            execution.setProcessBusinessKey(businessKey);
-        }
-        if(request.hasDublicate(businessKey)){
-            System.out.println("Working on existing request:" + businessKey);
-        }
-        Map<String, String> docData = new HashMap<>();
-        docData.put("requester", "John Doe");
-        docData.put("deadline", Timestamp.parseTimestamp("2020-09-25T00:00:00Z").toString());
-        docData.put("status", "OPEN");
-        docData.put("statusTimestamp", Timestamp.now().toString());
-        request.registerRequest(businessKey, docData);
+        System.out.println("Working with request:" + businessKey);
+        request.registerRequest(businessKey, null);
     }
   }
