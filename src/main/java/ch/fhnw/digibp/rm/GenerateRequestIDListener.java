f5b42ccc343683a0c7dad7cf0ce695068ce4d4d6
Anton Lorvi <lorvi.anton@gmail.com>
2019-05-18 22:27:32 +0200

    Test

--- src/main/java/ch/fhnw/digibp/rm/GenerateRequestIDListener.java
+++ src/main/java/ch/fhnw/digibp/rm/GenerateRequestIDListener.java
@@ -12,10 +12,11 @@ public class GenerateRequestIDListener implements ExecutionListener {
     public void notify(DelegateExecution execution) throws Exception {
         String requestID = execution.getBusinessKey();
         if(StringUtils.isEmpty(requestID)){
-            System.out.println(this.getClass().getSimpleName() + " - Working on a new request (generated id):" + requestID);
             requestID = UUID.randomUUID().toString();
             execution.setProcessBusinessKey(requestID);
             execution.setVariable("requestID", requestID);
+            execution.setVariable("submissionDate", new java.util.Date());
+            System.out.println(this.getClass().getSimpleName() + " - Working on a new request (generated id):" + requestID);
         }
     }
   }
