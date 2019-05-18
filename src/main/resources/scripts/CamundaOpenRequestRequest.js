b1223c8675e00206abd2c3b60743808786e24124
Anton Lorvi <lorvi.anton@gmail.com>
2019-05-18 21:58:14 +0200

    Test

---
+++ src/main/resources/scripts/CamundaOpenRequestRequest.js
@@ -0,0 +1,16 @@
+load('classpath:scripts/moment.js')
+out = JSON.stringify({
+    "requestID" : requestID,
+    "clientID" : clientID,
+    "yearEndDate" : moment(yearEndDate).format("DD.MM.YYYY"),
+    "emailAuditTeam" : emailAuditTeam,
+    "nameDataProvider" : nameDataProvider,
+    "emailDataProvider" : emailDataProvider,
+    "productSelection" : productSelection,
+    "deliveryDate" : moment(deliveryDate).format("DD.MM.YYYY"),
+    "coveredPeriod" : coveredPeriod,
+    "analysisDate" : moment(analysisDate).format("DD.MM.YYYY"),
+    "comparativeDate" : moment(comparativeDate).format("DD.MM.YYYY"),
+    "closureOfAccounts" : closureOfAccounts,
+    "freeText" : freeText
+});
